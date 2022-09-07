72b563110c161c0ae447b26539220c9fb57ebebc
Ubuntu <ubuntu@ip-172-31-44-116.ap-northeast-1.compute.internal>
2022-09-07 23:16:45 +0000

    password_maker

---
+++ practice.rb
@@ -0,0 +1,14 @@
+
+# inputs = gets.chomp
+# plus7 = "++++++++"
+# plus1 = "+"
+# output = "#{plus1}#{inputs}#{plus1}\n"
+# puts plus7
+# puts output
+# puts plus7
+
+
+a = ("A".."z").to_a.sample(5).join
+puts a.delete("aiueoAIUEO")
+# s = gets 
+# puts s.delete("aiueoAIUEO")
